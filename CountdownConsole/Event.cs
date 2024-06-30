e8bd6debf48d665138cdeb2375323ea380d1184b
Connor Proctor <ccjamp@gmail.com>
2024-06-30 19:19:19 -0400

    Add project files.

---
+++ CountdownConsole/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountdownConsole
+{
+    internal class Event
+    {
+        // Private field for ID
+        private readonly Guid _id;
+
+        // Constructor to initialize ID
+        public Event()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        // Property to get ID
+        public Guid ID
+        {
+            get { return _id; }
+        }
+
+        // Property to get and set Title
+        public string Title { get; set; }
+
+        // Property to get and set EndDate
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ CountdownConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using CountdownConsole;
+
+Console.WriteLine("Welcome to the Countdown Console!\n");
+
+List<Event> existingEvents = new List<Event>();
+Boolean exitApp = false;
+
+while (!exitApp)
+{
+    Console.WriteLine("What would you like to do?\n" +
+        $"0 - View Existing Events\n" +
+        $"1 - Add a New Event");
+
+    string userChoice = Console.ReadLine();
+
+    switch (userChoice)
+    {
+        case "0":
+            Console.WriteLine("You've chosen to view existing events.");
+            break;
+
+        case "1":
+            Console.WriteLine("You've chosen to add a new event.");
+            break;
+
+        default:
+            Console.WriteLine("Your choice selection was invalid.");
+            break;
+    }
+
+    Console.ReadLine();
+
+}
