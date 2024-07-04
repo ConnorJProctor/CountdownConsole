b8eacffc3de3d6abdfb72fb59acbcd39b046653d
Connor Proctor <ccjamp@gmail.com>
2024-07-04 11:18:30 -0400

    Added Days Remaining, remove event.  Improved menu.

--- CountdownConsole/Event.cs
+++ CountdownConsole/Event.cs
@@ -14,13 +14,14 @@ namespace CountdownConsole
         {
         }
 
-        // Property to get and set ID
         public int ID {  get; set; }
-
-        // Property to get and set Title
         public string Title { get; set; }
-
-        // Property to get and set EndDate
         public DateTime EndDate { get; set; }
+        public int DaysRemaining
+        {
+            get {
+                return (EndDate.Date - DateTime.Today).Days;
+            }
+        }
     }
 }
--- CountdownConsole/Program.cs
+++ CountdownConsole/Program.cs
@@ -9,20 +9,22 @@ Boolean exitApp = false;
 while (!exitApp)
 {
     Console.WriteLine("What would you like to do?\n" +
-        $"0 - View Existing Events\n" +
-        $"1 - Add a New Event");
+        $"E - View Existing Events\n" +
+        $"N - Add a New Event\n" +
+        $"R - Remove an Existing Event\n" +
+        $"Q - Quit Application");
 
-    string userChoice = Console.ReadLine();
+    string userChoice = Console.ReadLine().ToUpper();
 
     switch (userChoice)
     {
-        case "0":
+        case "E":
             Console.WriteLine("\nYou've chosen to view existing events.\n");
 
-            ConsoleTable table = new ConsoleTable("ID", "Title", "End Date");
+            ConsoleTable table = new ConsoleTable("ID", "Title", "End Date", "Days Remaining");
             foreach (Event e in existingEvents)
             {
-                table.AddRow(e.ID,e.Title,e.EndDate);
+                table.AddRow(e.ID,e.Title,e.EndDate,e.DaysRemaining);
             }
 
             table.Write();
@@ -30,7 +32,7 @@ while (!exitApp)
             break;
 
 
-        case "1":
+        case "N":
             Console.WriteLine("\nYou've chosen to add a new event.\n");
 
             Console.WriteLine("Please enter the title of the new event: ");
@@ -59,12 +61,39 @@ while (!exitApp)
             }
 
             Event newEvent = new Event();
-            newEvent.ID = newId;
-            newEvent.Title = newTitle;
-            newEvent.EndDate = newEndDate;
+            newEvent.ID             = newId;
+            newEvent.Title          = newTitle;
+            newEvent.EndDate        = newEndDate;
 
             existingEvents.Add(newEvent);
+            Console.WriteLine($"Added event '{newEvent.Title}' successfully!");
 
+            break;
+
+        case "R":
+            Console.WriteLine("\nYou've chosen to remove an existing event.\n");
+
+
+            Console.WriteLine("Please enter the ID of the event you would like to remove: ");
+            int idToRemove = Convert.ToInt32(Console.ReadLine());
+
+            Event selectedEvent = existingEvents.FirstOrDefault(e => e.ID == idToRemove);
+
+            if (selectedEvent != null)
+            {
+                existingEvents.Remove(selectedEvent);
+                Console.WriteLine($"Removed event '{selectedEvent.Title}' successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"Could not find an event with ID {idToRemove}");
+
+            }
+            break;
+
+
+        case "Q":
+            Environment.Exit(0);
             break;
 
         default:
@@ -72,6 +101,6 @@ while (!exitApp)
             break;
     }
 
-    Console.ReadLine();
+    Console.WriteLine();
 
 }
